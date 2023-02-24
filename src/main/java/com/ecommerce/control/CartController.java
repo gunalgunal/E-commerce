275aa965770a3a2f87969dbaf5e9a2c32b940dc9
gunalgunal <seelanguna530@gmail.com>
2023-02-24 10:11:23 +0530

    fourth commit

--- src/main/java/com/ecommerce/control/CartController.java
+++ src/main/java/com/ecommerce/control/CartController.java
@@ -1,2 +1,31 @@
-package com.ecommerce.control;public class CartController {
+package com.ecommerce.control;
+
+
+import com.ecommerce.entity.ProductEntity;
+import com.ecommerce.repository.ProductRepository;
+import com.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+
+@Controller
+public class CartController {
+
+    private static ArrayList<ProductEntity> cart=new ArrayList<>();
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("addToCart/{id}")
+    public String cardAdding(@PathVariable int id)
+    {
+       cart.add(productRepository.findById(id).get());
+       return "redirect:/shop";
+    }
+
+
 }
--- src/main/java/com/ecommerce/control/LoginController.java
+++ src/main/java/com/ecommerce/control/LoginController.java
@@ -1,9 +1,31 @@
 package com.ecommerce.control;
 
+import com.ecommerce.entity.Role;
+import com.ecommerce.entity.UserInformation;
+import com.ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class LoginController {
+
+    private UserRepository userRepository;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+    public LoginController(UserRepository userRepository)
+    {
+        this.userRepository=userRepository;
+    }
 
 
     @GetMapping("/login")
@@ -16,5 +38,18 @@ public class LoginController {
     public String registerPage()
     {
         return "register";
+    }
+    @PostMapping("/register")
+    public String registerTheUser(@ModelAttribute("user") UserInformation user, HttpServletRequest request) throws ServletException {
+        String password=user.getPassword();
+        user.setPassword(passwordEncoder.encode(password));
+        Role role=new Role();
+        role.setRole("USER");
+        List<Role> roleList=new ArrayList<>();
+        roleList.add(role);
+        user.setRoles(roleList);
+        userRepository.save(user);
+        request.login(user.getUserName(),password);
+        return "redirect:/";
     }
 }
